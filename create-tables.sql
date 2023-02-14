5915bc2ddf30be421e0775520fa9b2fdebf747f5
Yingying Zhang <noreply>
2023-02-13 20:38:30 -0800

    data

--- create-tables.sql
+++ create-tables.sql
@@ -8,3 +8,29 @@ CREATE Table phone (
     Contact INTEGER,
     Type VARCHAR
 );
+
+CREATE TABLE registration (
+    TransactionDate DATE,
+    Make VARCHAR,
+    Model VARCHAR,
+    MYear INTEGER,
+    Color VARCHAR,
+    Type VARCHAR,
+    Use VARCHAR,
+    FuelTypePrimary VARCHAR,
+    GVWRClass VARCHAR,
+    GVWRRange VARCHAR,
+    FuelTypeSecondary VARCHAR,
+    Electrification VARCHAR,
+    PlateBackground VARCHAR,
+    PlateConfiguration VARCHAR,
+    OwnerType VARCHAR,
+    County VARCHAR,
+    State VARCHAR,
+    PostalCode VARCHAR,
+    TransactionType VARCHAR,
+    TransactionChannel VARCHAR,
+    PaymentType VARCHAR,
+    CensusTract VARCHAR,
+    TransactionCount VARCHAR
+);
